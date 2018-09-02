9fb619354d51ffe1cc6e188dc9af69fa73d782c3
Mark <markjcarroll@hotmail.co.uk>
2018-09-02 11:08:49 +0100

    added if statement to detect time of day for a given peramiter

--- Website_Blocker/Website_Blocker/Website_Blocker.py
+++ Website_Blocker/Website_Blocker/Website_Blocker.py
@@ -1,7 +1,11 @@
 import time
+from datetime import datetime as dt
 host_path="c:\Windows\System32\drivers\etc\hosts"
 redirect="127.0.0.1"
 website_list=["www.facebook.com","facebook.com"]
 
 while True:
-    time.sleep(5)
+    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < (dt.now().year,dt.now().month,dt.now().day,16):
+        print("Working hours...")
+
+    time.sleep(5)    
